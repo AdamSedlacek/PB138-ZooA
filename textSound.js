55d46c93a5191f1a7a700bcfe4606f4bcdf359eb
jima88 <kamila.chytalova@gmail.com>
2017-05-23 16:28:16 +0200

    Add files via upload
    Přidány třídy pro tlačítka (sdílení, přehrávání zvuku a textu). Testování třídy index.android (pomocí HelloWorld!).

---
+++ textSound.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+const React = require('react');
+const ReactNative = require('react-native');
+const {
+  Alert,
+  Button,
+  View,
+} = ReactNative;
+
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
+
+exports.displayName = 'ButtonExample';
+exports.framework = 'React';
+exports.title = '<Button>';
+exports.description = 'Simple React Native button component.';
+
+
+exports.examples = [
+{
+    title: 'Fit to text layout',
+    description: 'This layout strategy lets the title define the width of ' +
+      'the button',
+    render: function() {
+      return (
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Button
+            onPress={onButtonPress}
+            title="Přehrát text"
+            //accessibilityLabel="This sounds great!"
+          />
+          <Button
+            onPress={onButtonPress}
+            title="Přehrát znovu"
+            color="#841584"
+            //accessibilityLabel="Ok, Great!"
+          />
+        </View>
+      );
+    },
+  }
+];
