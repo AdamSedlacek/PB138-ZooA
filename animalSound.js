3021529e5ffa6eb32998fa758a952e8d34526cdc
AdamSedlacek <adams.sedlacek@gmail.com>
2017-05-26 09:30:04 +0200

    Update animalSound.js
    Tlačítko použito z https://github.com/ide/react-native-button
    Nejprve potřeba naistalovat pomocí: npm install react-native-button --save
    Poté importovat: import Button from 'react-native-button'

--- animalSound.js
+++ animalSound.js
@@ -1,43 +1,21 @@
-// JavaScript Document
-const React = require('react');
-const ReactNative = require('react-native');
-const {
-  Alert,
-  Button,
-  View,
-} = ReactNative;
+import React, { Component } from 'react';
+import Button from 'react-native-button';
 
-const onButtonPress = () => {
-  Alert.alert('Button has been pressed!');
-};
-
-exports.displayName = 'ButtonExample';
-exports.framework = 'React';
-exports.title = '<Button>';
-exports.description = 'Simple React Native button component.';
-
-
-exports.examples = [
-{
-    title: 'Fit to text layout',
-    description: 'This layout strategy lets the title define the width of ' +
-      'the button',
-    render: function() {
-      return (
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Button
-            onPress={onButtonPress}
-            title="Zvuk zvířete"
-            //accessibilityLabel="This sounds great!"
-          />
-          <Button
-            onPress={onButtonPress}
-            title="Přehrát znovu"
-            color="#841584"
-            //accessibilityLabel="Ok, Great!"
-          />
-        </View>
-      );
-    },
+export default class ExampleComponent extends Component {
+  constructor(props, context) {
+    super(props, context);
   }
-];
+  _handlePress() {
+    console.log('Pressed!');
+  }
+  render() {
+    return (
+      <Button
+        style={{fontSize: 20, color: 'green'}}
+        styleDisabled={{color: 'red'}}
+        onPress={() => this._handlePress()}>
+        Přehrát zvuk zvířátka!
+      </Button>
+    );
+  }
+};
