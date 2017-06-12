0a1b1396316ee200a261580838145a30dbacb862
KlaraKosecka <koseckaklara@gmail.com>
2017-06-12 13:20:42 +0200

    Updated text sound button.

--- components/textSound.js
+++ components/textSound.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import Button from 'react-native-button';
+import {Button} from 'react-native';
 
 export default class ExampleComponent extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-  _handlePress() {
-    console.log('Pressed!');
-  }
-  render() {
-    return (
-      <Button
-        style={{fontSize: 20, color: 'green'}}
-        styleDisabled={{color: 'red'}}
-        onPress={() => this._handlePress()}>
-        Přehrát text!
-      </Button>
-    );
-  }
+    constructor(props, context) {
+        super(props, context);
+    }
+    _handlePress() {
+        console.log('Pressed!');
+    }
+    render() {
+        return (
+                <Button
+                color='green'
+                title="Přehrát text!"
+                onPress={() => this._handlePress()}>
+                </Button>
+                );
+    }
 };
