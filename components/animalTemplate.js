d9dbd4888173ab4305d396b6b46e38a606407964
AdamSedlacek <adams.sedlacek@gmail.com>
2017-06-12 13:37:44 +0200

    Add files via upload

---
+++ components/animalTemplate.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View} from 'react-native';
+import styles from '../styles/styles';
+import InPageImage from './inPageImage';
+
+export default class AnimalTemplate extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+      return (
+        <View>
+          <View>
+            <InPageImage firstImage={true} indexes={this.props.firstIndex} thumbnails={this.props.thumbnails} images={this.props.images} />
+          </View>
+          <View style={styles.scrollView}>
+            {this.props.children}
+          </View>
+        </View>
+      );
+    }
+};
+
+AnimalTemplate.propTypes = {
+    firstIndex: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
+    thumbnails: React.PropTypes.array.isRequired,
+    images: React.PropTypes.array.isRequired,
+    children: React.PropTypes.arrayOf(React.PropTypes.element).isRequired,
+};
