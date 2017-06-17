2a1c38e8d62d64f2de6e8979dbbad1f1c8ca0e44
KatyZ <katyzubnarova@gmail.com>
2017-06-17 21:30:05 +0200

    Pridanie pop-up notifikacie o sluchatkach

--- components/soundButton.js
+++ components/soundButton.js
@@ -1,38 +1,80 @@
 import React, {Component} from 'react';
 import {Button} from 'react-native';
-const Sound = require('react-native-sound');
-var isPlaying = false;
-var textS;
+import PushNotification from 'react-native-push-notification';
 
+const Sound = require('react-native-sound');
+
+
+/**
+* Class SoundButton
+* creates a button for playing a sound
+* need to get the sound in props: this.props.sound
+*/
 export default class SoundButton extends React.Component {
+
+    /**
+    * Constructor
+    * sets showAlert and isPlaying
+    * configure the PushNotification
+    */
     constructor(props, context) {
         super(props, context);
+
+        this.showAlert = true;
+        /** for all rendered buttons will be the alert shown once.
+        *If you want to show the alert for the whole app only once,
+        *then the showAlert should be defined right after imports,
+        *then you need to use it without preposition this. in this whole class
+        */
+        this.isPlaying = false;
+
+        PushNotification.configure({
+            onNotification: function(notification) {
+                Alert.alert(notification.message);
+            },
+
+            permissions: {
+                alert: true,
+                badge: false,
+                sound: false // this doesn´t work. Why?
+            },
+        });
     }
+
     /**
      * 1st function of the button
-     * plays the sound
+     * plays the sound and shows a pop-up alert (only when playd 1st time)
      */
-    _handlePress1() {
-         console.log('PressedStart!', isPlaying);
-         this.isPlaying = true;
-         this.forceUpdate();
-         this.textS = new Sound(this.props.sound, (error) => {
-              if (error) {
-                    console.log('failed to load the sound', error);
-              } else {
-                   this.textS.play((success) => {
-                        if (success) {
-                             console.log('successfully finished playing');
-                             this.isPlaying = false;
-                        } else {
-                             console.log('playback failed due to audio decoding errors');
-                        }
-                        this.textS.release();
-                        this.forceUpdate();
-                   });
-                }
-            });
-    }
+     _handlePress1() {
+             console.log('PressedStart!');
+             if (this.showAlert){
+                let z = {
+                     title: 'Použijte prosím sluchátká!',
+                     vibrate: true,
+                     message: ('Hlasitý odposlech ruší zvířata.'),
+                   };
+                   PushNotification.localNotification(z);
+                   this.showAlert = false;
+                   }
+             this.isPlaying = true;
+             this.forceUpdate();
+             this.textS = new Sound(this.props.sound, (error) => {
+                  if (error) {
+                        console.log('failed to load the sound', error);
+                  } else {
+                       this.textS.play((success) => {
+                            if (success) {
+                                 console.log('successfully finished playing');
+                                 this.isPlaying = false;
+                            } else {
+                                 console.log('playback failed due to audio decoding errors');
+                            }
+                            this.textS.release();
+                            this.forceUpdate();
+                       });
+                    }
+                });
+        }
 
     /**
      * 2nd function of the button
